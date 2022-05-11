ea76c6b8cde0e2549f4d93fa2a584afa9ade92aa
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2022-05-11 17:12:44 -0500

    Update user.js

--- server/user.js
+++ server/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const { mongooseConnectionString } = require('./config/appconfig.json')
 
+
 mongoose.connect(mongooseConnectionString).then(() => console.log(`Connected to db`))
 
 let Schema = new mongoose.Schema({
